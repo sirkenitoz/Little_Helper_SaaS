0010baca9ff97e763424cce1312fb406afe4b825
Your Name <your.email@example.com>
2024-01-30 13:58:10 +0300

    New Dashboard content added successfully

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { 
@@ -14,7 +15,6 @@ import {
     Settings,
     VideoIcon
  } from "lucide-react";
-
 
 const montserrat = Montserrat ({
     weight: "600",
@@ -66,6 +66,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -86,7 +87,7 @@ const Sidebar = () => {
                         <Link
                         href={route.href}
                         key={route.href}
-                        className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                        className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
                         >
                             <div className="flex items-center flex-1">
                               <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
